364cc813e0a163fd58e10bc796bd33d6ab0a470c
Nacho258258 <ignacioma.alvarez@gmail.com>
2021-11-21 20:51:34 -0300

    Cambios menores

--- Update_merc.xaml.cs
+++ Update_merc.xaml.cs
@@ -28,44 +28,76 @@ namespace Gestion
 
 
 
-            if (MuestraMercUpdate.IsMouseOver==true)
-            {
-                TbTipoproducto.Text = MuestraMercUpdate.Columns[1].ToString();
-            } 
-            //TbTipoproducto.Text = "mono";
-            //TbTipoproducto.Text = MuestraMercUpdate.CurrentCell.ToString();
+
+
 
         }
 
-        
+
 
 
         private void Muestra_MercUpdate()
+        {
+
+            ConnectionDB connection = new ConnectionDB();
+
+            string consulta = "select * from Mercaderias as MuestraInfo";
+
+
+            SqlDataAdapter miadapatadorsql = new SqlDataAdapter(consulta, connection.Connection);
+
+            using (miadapatadorsql)
             {
+                DataTable mydatable = new DataTable();
 
-                ConnectionDB connection = new ConnectionDB();
+                miadapatadorsql.Fill(mydatable);
 
-                string consulta = "select * from Mercaderias as MuestraInfo";
-
-
-                SqlDataAdapter miadapatadorsql = new SqlDataAdapter(consulta, connection.Connection);
-
-                using (miadapatadorsql)
-                    {
-                         DataTable mydatable = new DataTable();
-
-                         miadapatadorsql.Fill(mydatable);
-
-                        MuestraMercUpdate.DisplayMemberPath = "MuestraInfo";
-                        MuestraMercUpdate.SelectedValuePath = "id";
-                        MuestraMercUpdate.ItemsSource = mydatable.DefaultView;
+                MuestraMercUpdate.DisplayMemberPath = "MuestraInfo";
+                MuestraMercUpdate.SelectedValuePath = "id";
+                MuestraMercUpdate.ItemsSource = mydatable.DefaultView;
 
 
-                    }
+                
+            }
 
 
-            
 
-        }  
+
+        }
+
+      
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           // TbTipoproducto.Text = MuestraMercUpdate.Columns[0].GetCellContent(MuestraMercUpdate.SelectedIndex).ToString();
+
+               // [MuestraMercUpdate.SelectedIndex]["Tipo_Producto"].ToString();
+
+        }
+
+
+        //TODO
+        //Ver como sacar el id para hacer el update.
+        private void BtUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectionDB connection = new ConnectionDB();
+
+            string consulta = "Update Mercaderias set Nombre=@Nombre, cantidad=@cantidad, precio=@precio where id = " + MuestraMercUpdate.SelectedItem.ToString();
+
+            SqlCommand mysqlcommand = new SqlCommand(consulta, connection.Connection);
+            connection.Connection.Open();
+
+           // mysqlcommand.Parameters.AddWithValue("@Tipo_Producto", TbTipoproducto.Text);
+            mysqlcommand.Parameters.AddWithValue("@Nombre", TbNombre.Text);
+            mysqlcommand.Parameters.AddWithValue("@cantidad", TbCantidad.Text);
+            mysqlcommand.Parameters.AddWithValue("@precio", TbPrecio.Text);
+
+            mysqlcommand.ExecuteNonQuery();
+
+            connection.Connection.Close();
+
+            Muestra_MercUpdate();
+        }
     }
 }
